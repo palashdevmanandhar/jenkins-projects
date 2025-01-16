793b3ad06cb4dae666a3e6a2da8e6b1d89968e01
palashdevmanandhar <palash.manandhar@gmail.com>
2025-01-16 13:23:46 +0000

    bas script to create s3 bucket and dynamodb added

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "ec2_public_ip_stage" {
-  value = aws_instance.staging_instance.public_ip
+  value       = aws_instance.staging_instance.public_ip
   description = "The public IP of the staging EC2 instance"
 }
 
 output "ec2_public_ip_prod" {
-  value = aws_instance.production_instance.public_ip
+  value       = aws_instance.production_instance.public_ip
   description = "The public IP of the staging EC2 instance"
 }
