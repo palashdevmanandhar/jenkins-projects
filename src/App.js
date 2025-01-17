a2b7dd25ab35868865adefb6b92577d01d550ab7
palashdevmanandhar <palash.manandhar@gmail.com>
2025-01-17 09:13:22 +0000

    changing jenkins file to get ip's again

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Making slight change to react app after adding
+          REact app
         </a>
       </header>
     </div>
