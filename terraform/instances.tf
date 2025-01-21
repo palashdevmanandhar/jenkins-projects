6cb85e47abd7fd5ce2209cc09738e9fbd21a02c1
palashdevmanandhar <palash.manandhar@gmail.com>
2025-01-21 09:38:55 +0000

    2 prod instances registered as target for alb

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -58,6 +58,46 @@ resource "aws_security_group" "sg_region1" {
   }
 }
 
+resource "aws_security_group" "prod_sg" {
+  provider    = aws.region1
+  name        = "prod-instance-security-group"
+  description = "Security group for EC2 instances in target group"
+  vpc_id      = aws_vpc.vpc_region1.id
+
+  # Allow inbound traffic only from ALB
+  ingress {
+    description     = "Allow HTTP from ALB"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  # Allow SSH access for management
+  ingress {
+    description = "Allow SSH from all"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Consider restricting this to your IP range
+  }
+
+  # Egress Rules
+  egress {
+    description = "Allow all egress"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "prod-instance-security-group"
+    project = var.project_name
+    region  = var.region1
+  }
+}
+
 resource "aws_instance" "staging_instance" {
   provider                    = aws.region1
   ami                         = var.aws_ami_id_region1
@@ -87,7 +127,7 @@ resource "aws_instance" "staging_instance" {
   }
 }
 
-resource "aws_instance" "production_instance_region1" {
+resource "aws_instance" "production_instance_node1" {
   provider                    = aws.region1
   ami                         = var.aws_ami_id_region1
   instance_type               = "t2.micro"
@@ -95,7 +135,7 @@ resource "aws_instance" "production_instance_region1" {
   subnet_id                   = aws_subnet.public_subnet_region1.id
 
   # Security Group (optional, add an existing SG or use Terraform to create one)
-  vpc_security_group_ids = [aws_security_group.sg_region1.id]
+  vpc_security_group_ids = [aws_security_group.prod_sg.id]
 
   # Key Pair for SSH Access
   key_name = aws_key_pair.key_pair_region1.key_name
@@ -108,7 +148,7 @@ resource "aws_instance" "production_instance_region1" {
 
   # Add Tags
   tags = {
-    Name     = "production_instance_region1"
+    Name     = "production_instance_node1"
     project  = var.project_name
     env      = "prod"
     function = "webserver"
@@ -116,7 +156,36 @@ resource "aws_instance" "production_instance_region1" {
   }
 }
 
-resource "aws_instance" "jenkins_instance" {
+resource "aws_instance" "production_instance_node2" {
+  provider                    = aws.region1
+  ami                         = var.aws_ami_id_region1
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.public_subnet_region1_az2.id
+
+  # Security Group (optional, add an existing SG or use Terraform to create one)
+  vpc_security_group_ids = [aws_security_group.prod_sg.id]
+
+  # Key Pair for SSH Access
+  key_name = aws_key_pair.key_pair_region1.key_name
+
+  # Add a basic block device (root volume)
+  root_block_device {
+    volume_size = 8 # 8GB root volume
+    volume_type = "gp3"
+  }
+
+  # Add Tags
+  tags = {
+    Name     = "production_instance_node2"
+    project  = var.project_name
+    env      = "prod"
+    function = "webserver"
+    region   = var.region1
+  }
+}
+
+resource "aws_instance" "jenkins_control" {
   provider                    = aws.region1
   ami                         = var.aws_ami_id_region1
   instance_type               = "t2.medium"
@@ -137,10 +206,11 @@ resource "aws_instance" "jenkins_instance" {
 
   # Add Tags
   tags = {
-    Name     = "jenkins-server"
+    Name     = "jenkins_control"
     project  = var.project_name
     function = "jenkins"
     region   = var.region1
+    type     = "control"
   }
 }
 
@@ -185,6 +255,14 @@ resource "aws_security_group" "sg_region2" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Allow HTTP from all"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # Egress Rules
   egress {
     description = "Allow all egress"
@@ -201,10 +279,10 @@ resource "aws_security_group" "sg_region2" {
   }
 }
 
-resource "aws_instance" "production_instance_region2" {
+resource "aws_instance" "jenkins_node1" {
   provider                    = aws.region2
   ami                         = var.aws_ami_id_region2
-  instance_type               = "t2.micro"
+  instance_type               = "t2.medium"
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.public_subnet_region2.id
 
@@ -216,18 +294,19 @@ resource "aws_instance" "production_instance_region2" {
 
   # Add a basic block device (root volume)
   root_block_device {
-    volume_size = 8 # 8GB root volume
+    volume_size = 20 # 8GB root volume
     volume_type = "gp3"
   }
 
   # Add Tags
   tags = {
-    Name     = "production_instance_region2"
+    Name     = "jenkins_node1"
     project  = var.project_name
-    env      = "prod"
-    function = "webserver"
+    function = "jenkins"
     region   = var.region2
+    type     = "node"
   }
 }
+
 
 ######### End of instances for Region2 ########
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,18 +3,23 @@ output "ec2_public_ip_stage" {
   description = "The public IP of the staging EC2 instance"
 }
 
-output "ec2_jenkins" {
-  value       = aws_instance.jenkins_instance.public_ip
+output "ec2_jenkins_control" {
+  value       = aws_instance.jenkins_control.public_ip
   description = "The public IP of the jenkins EC2 instance"
 }
 
-output "ec2_public_ip_prod_region1" {
-  value       = aws_instance.production_instance_region1.public_ip
+output "ec2_jenkins_node" {
+  value       = aws_instance.jenkins_node1.public_ip
+  description = "The public IP of the jenkins EC2 instance"
+}
+
+output "ec2_public_ip_prod_node1" {
+  value       = aws_instance.production_instance_node1.public_ip
   description = "The public IP of the staging EC2 instance"
 }
 
-output "ec2_public_ip_prod_region2" {
-  value       = aws_instance.production_instance_region2.public_ip
+output "ec2_public_ip_prod_node2" {
+  value       = aws_instance.production_instance_node2.public_ip
   description = "The public IP of the staging EC2 instance"
 }
 
