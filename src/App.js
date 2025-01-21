50e5253e9a1e53e91b0895e1455e62861a3ffa49
palashdevmanandhar <palash.manandhar@gmail.com>
2025-01-21 15:51:51 +0000

    ip address printing of server removed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
-import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [ipAddress, setIpAddress] = useState('Loading...');
-
-  useEffect(() => {
-    // Using ipify API to get the public IP address
-    fetch('https://api.ipify.org?format=json')
-      .then(response => response.json())
-      .then(data => setIpAddress(data.ip))
-      .catch(error => {
-        console.error('Error fetching IP:', error);
-        setIpAddress('Failed to load IP');
-      });
-  }, []);
 
   return (
     <div className="App">
@@ -24,7 +11,6 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p className="text-xl">
-          Server IP Address: <code>{ipAddress}</code>
         </p>
         <a
           className="App-link"
